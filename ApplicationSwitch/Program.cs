d2a4a790b3637d3ba953088482148ce80d7e238f
tqji31001 <tqji31001@gmail.com>
2024-12-30 20:27:42 +0900

    check update code.

--- ApplicationSwitch/Program.cs
+++ ApplicationSwitch/Program.cs
@@ -9,22 +9,14 @@ if (!Directory.Exists(setting.LogDirectory))
 {
     Directory.CreateDirectory(setting.LogDirectory);
 }
-Logger.LogFile =
-    Path.Combine(setting.LogDirectory, "AppSwitch_" + Path.Combine(DateTime.Now.ToString("yyyyMMdd")) + ".log");
+Logger.LogFile = Path.Combine(setting.LogDirectory, "AppSwitch_" + Path.Combine(DateTime.Now.ToString("yyyyMMdd")) + ".log");
 Logger.WriteRaw("--------------------------------\r\n");
 Logger.WriteLine("Start Application switch.", 0);
-
 
 //  Main process
 AppRoot.LoadSettingFiles(setting.ConfigDirectory).
     ToList().
     ForEach(app => app.ProcessRules(setting.EvacuateDirectory));
-
-
-
-//var path = @"..\..\..\Test\AppConfig04.yml";
-//var app = DataSerializer.Load<AppRoot>(path);
-//app.ProcessRules(setting.EvacuateDirectory);
 
 //  End process
 Logger.WriteLine("End Application switch.", 0);
